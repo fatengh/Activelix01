8d7ccebd086f18c86af1ffc967ee7d892d178893
fatengh <faten.arg@hotmail.com>
2020-08-18 00:59:06 +0300

    setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-export ALGORITHMS=['RS256']
-export API_AUDIENCE='capstoneapi'
-export AUTH0_DOMAIN='coffepro.us.auth0.com'
-export DATABASE_URL='postgres://postgres:6210665@localhost:5432/activelix'
+export DATABASE_URL='postgresql://postgres:6210665@localhost:5432/activelix'
+export AUTH0_CLIENT_ID = 'dzv5kEK7CtaJ7LL7N5iwSVUF2i0dtaed'
+export AUTH0_CLIENT_SECRET = 'ML8LqNRsRvK3Hbeagol4KQTkP8TfgvAXPHZFccEh45qOIhVB698JeGeoMDoIzmL9'
+export AUTH0_DOMAIN = 'coffepro.us.auth0.com'
+export ENV='development'
+export FLASK_APP=app
+export FLASK_DEBUG=True
